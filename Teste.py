779a6fc671deb4df5318aa19ac7faa4bd56089ce
LucasGonCastro <lucsgoncastro@gmail.com>
2025-04-29 17:56:53 -0300

    main

--- Teste.py
+++ Teste.py
@@ -1,1 +1,13 @@
-print("Hello World")
+
+
+print("Calculo de Area de Circulo")
+
+Raio = 5.23
+
+area = 3.14 * Raio **2
+
+print("Area do circulo,", area)
+
+print(f'Area do circulo e do Raio {Raio} e do Circulo {area}')
+
+print(f'A Area do Cicrculo de raio{Raio} e da Circulo {area:.4f}')
---
+++ definição de variavel Charles.py
@@ -0,0 +1,10 @@
+print("Notas Do Semestre")
+
+nota1 = 6.5
+nota2 = 7.0
+nota3 = 4.9
+nota4 = 6.3
+
+media = (nota1 + nota2 + nota3 + nota4)/4
+
+print(f"Media={media:.1f}")
