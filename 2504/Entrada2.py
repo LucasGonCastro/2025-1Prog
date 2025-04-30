29a673f0bce6b90f5b869f93481886f0084a265f
LucasGonCastro <lucsgoncastro@gmail.com>
2025-04-30 17:54:52 -0300

    main

--- 2504/Entrada2.py
+++ 2504/Entrada2.py
@@ -1,8 +1,13 @@
 #Entrada 2
 
-valor_venda = float(input("Digite o Valor da Venda R$"))
-percentual = float(input("Informe a Comissão R%"))
+valor_venda = float(input("Digite o Valor da Venda R$: "))
+percentual = float(input("Informe a Comissão R%: "))
 
 comissao = valor_venda * percentual / 100
 
 print(f"O valor da comissão é R${comissao:.2f}")
+
+#Nota 1 : Ler Nota de Aula 4,5
+#Nota 2 : Valor Negativo
+#Nota 3 : Teste com Comissoes Positivas
+#Nota 4 : Tratamento de Erro
