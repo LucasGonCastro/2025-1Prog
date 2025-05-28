aacdefa18a394c6fead4bad3c132ff0b0c71f8b1
LucasGonCastro <lucsgoncastro@gmail.com>
2025-05-28 17:33:08 -0300

    maun

---
+++ 2705/While.py
@@ -0,0 +1,7 @@
+Numero = int(input("Digite um Numero"))
+Multiplicando = 1
+while Multiplicando <= 10:
+    print(f"{Numero} x {Multiplicando} = {Multiplicando * Numero}")
+    Multiplicando += 1 
+print("Fim Da tabuada")
+
